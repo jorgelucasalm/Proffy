d03017b8d8b5f895a2f33d792ca2bf60901953da
Jorge Lucas <JorgeLucas006@gmail.com>
2020-08-05 18:22:19 -0300

    Configurando navegações entre Rotas
    Adicionando Routes na aplicação e termino do Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Landing from './pages/Landing'
+import Routes from './routes'
+
+import './assets/styles/global.css'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './style.css'
+
+import logoimg from '../../assets/images/logo.svg';
+import landingimg from '../../assets/images/landing.svg'
+
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoimg} alt="Proffy" />
+                    <h1>Sua plataforma de estudos online.</h1>
+                </div>
+
+                <img
+                    src={landingimg}
+                    alt="Plataforma de estudos"
+                    className="hero-image"
+                />
+
+                <div className="buttons-container">
+                    <a href="/study" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </a>
+
+                    <a href="/give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                        Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coraçao Roxo" />
+                </span>
+
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
