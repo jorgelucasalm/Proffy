4a1f84af04d4d68c0489ce19ac3e89aaf1bfa924
Jorge Lucas <JorgeLucas006@gmail.com>
2020-08-09 18:55:12 -0300

    feature(server): criação de rota para cadastro

---
+++ server/src/database/utils/convertHoursToMinutes.ts
@@ -0,0 +1,10 @@
+
+
+export default function converctHoursToMinutes(time: string){
+
+    const [hour, minutes] = time.split(':').map(Number);
+    const timeInMinutes  = (hour * 60) + minutes;
+
+    return timeInMinutes;
+
+};
